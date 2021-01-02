d6c41079b5788c69bc16320602afd3cfe2ec6fba
muhammad-rosyid <muh.rosyid95@gmail.com>
2021-01-02 13:00:18 +0700

    commit

--- index.php
+++ index.php
@@ -49,7 +49,14 @@
             <div class="container">
                 <div class="text-center">
                     <h2 class="section-heading text-uppercase">pengendali & pemantau</h2>
-                    <h3 class="section-subheading text-muted">13 November 2020 | 21:06 WIB</h3>
+                    <h3 class="section-subheading text-muted"> 
+                        <div id="j"></div> 
+                        <script>
+                        var d = new Date();
+                        document.getElementById("j").innerHTML = d;
+                        </script>
+
+                        </h3>
                 </div>
                 <div class="row text-center">
                     <div class="col-md-4">
